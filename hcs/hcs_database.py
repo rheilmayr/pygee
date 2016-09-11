c7e7e3015bd6ce0bba1ffc0e519f4226030f6fd5
rheillmayr <rheilmayr@gmail.com>
2016-09-11 14:48:07 -0700

    Module to hold dictionary definitions of fusion table locations

---
+++ hcs/hcs_database.py
@@ -0,0 +1,9 @@
+maps_dict = {'crgl_stal': 'ft:12lUjvcMXXuSrSS4w1eMVCgjvTjksKY7FS453eHda',
+             'app': '',
+             'wilm_cal': 'ft:1eHm5seFIE_CydzRgNenfAj20Qkh_SlqQkEtbxcLH',
+             'gar_pgm': 'ft:1YG1x2BRB63C4CRY6oBUwUxzbo569IN37nW9RMcA0'}
+
+plots_dict = {'crgl_stal': '',
+              'app': '',
+              'wilm_cal': '',
+              'gar_pgm': ''}
